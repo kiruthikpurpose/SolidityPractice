6b2f380e012423d12beec356129fbc4a183a8b8f
Kiruthik <147307440+kiruthikpurpose@users.noreply.github.com>
2024-09-15 12:18:26 +0530

    Create SmartContractCounter.sol

---
+++ SmartContractCounter.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 public count;
+
+    function increment() public {
+        count += 1;
+    }
+
+    function decrement() public {
+        require(count > 0, "Counter is already at zero");
+        count -= 1;
+    }
+
+    function reset() public {
+        count = 0;
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
